a71050d6ee8db4354d47db6baf963f55dd46506d
Jennifer Deleon <jenndeleon404@gmail.com>
2021-10-08 11:10:57 -0500

    web exercise

---
+++ js/JS-with-html.js
@@ -0,0 +1,14 @@
+"use strict"
+var jeff = "Jeff";
+console.log(jeff);
+
+console.log("Hello, This is it inserted into the HTML!");
+
+var returnOfAlert = alert("TGIF! Thank goodness I'm Fred!");
+console.log(returnOfAlert);
+
+var returnOfConfirm = confirm("Got any plans for the weekend?");
+console.log(returnOfConfirm);
+
+var favoriteNuMetalBand = prompt("What is you favorite Nu metal band?");
+console.log("Ha ha, you like " + favoriteNuMetalBand + "!");

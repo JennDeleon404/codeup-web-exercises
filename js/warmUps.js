b4df7b9f2b243e41033fa74487e1b1532f675614
Jennifer Deleon <jenndeleon404@gmail.com>
2021-10-19 09:26:44 -0500

    its not working

--- js/warmUps.js
+++ js/warmUps.js
@@ -1,11 +1,26 @@
-function typeCheck(input){
-    if (typeof input === 'string'){
-        return "Haha tghats a string of a string"
-    }else{
-        return typeof input;
+// function typeCheck(input){
+//     if (typeof input === 'string'){
+//         return "Haha thats a string of a string"
+//     }else{
+//         return typeof input;
+//     }
+// }
+// console.log('');
+// console.log(isNaN());
+// typeCheck(!false * 3);
+// typeCheck(4 + ‘4’);
+
+//write a program that console.logs the numbers from 1 - 100. But for the multiples of three console.log  "fizz" instead of the number and for the multiples of five console.log 'Buzz'. For numbers which are multiples of both 3 and 5 console.log "FizzBuzz"
+
+for (var fiz = 1; fiz <= 100; fiz++){
+    if(fiz % 3 === 0 && fiz % 5 === 0){
+        console.log("FizzBUzz");
+    }else if (fiz % 3 === 0){
+        console.log("Fizz");
+    }else (buzz % 5 === 0)
+    {
+        console.log("Buzz");
+    }else {
+        console.log(fiz)
     }
 }
-console.log('');
-console.log(isNaN());
-typeCheck(!false * 3);
-typeCheck(4 + ‘4’);

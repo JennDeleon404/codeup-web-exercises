ceecb4166eab5131f8e4b959d9fc0fddf7f201b2
Jennifer Deleon <jenndeleon404@gmail.com>
2021-10-20 10:46:11 -0500

    message

--- js/planets-array.js
+++ js/planets-array.js
@@ -20,21 +20,32 @@
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
+    planets.unshift("The Sun");
 
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
+    planets.push("Pluto");
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
+    planets.shift();
+    console.log(planets +"The Sun");
 
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
+    planets.pop(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    planets.sort();
+    console.log("Earth");
 
     console.log("Reversing the order of the planets array.");
+    console.log(planets);
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
     console.log(planets);
+    planets.sort();
+    console.log(planets)
 })();

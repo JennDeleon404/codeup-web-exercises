23545cb066a7fd6bfa732570daea839b6d340fbe
Jennifer Deleon <jenndeleon404@gmail.com>
2021-10-20 09:18:48 -0500

    progress

--- js/warmUps.js
+++ js/warmUps.js
@@ -11,16 +11,21 @@
 // typeCheck(4 + ‘4’);
 
 //write a program that console.logs the numbers from 1 - 100. But for the multiples of three console.log  "fizz" instead of the number and for the multiples of five console.log 'Buzz'. For numbers which are multiples of both 3 and 5 console.log "FizzBuzz"
+//
+// for (var fiz = 1; fiz <= 100; fiz++){
+//     if(fiz % 3 === 0 && fiz % 5 === 0){
+//         console.log("FizzBUzz");
+//     }else if (fiz % 3 === 0){
+//         console.log("Fizz");
+//     }else (buzz % 5 === 0)
+//     {
+//         console.log("Buzz");
+//     }else {
+//         console.log(fiz)
+//     }
+// }
 
-for (var fiz = 1; fiz <= 100; fiz++){
-    if(fiz % 3 === 0 && fiz % 5 === 0){
-        console.log("FizzBUzz");
-    }else if (fiz % 3 === 0){
-        console.log("Fizz");
-    }else (buzz % 5 === 0)
-    {
-        console.log("Buzz");
-    }else {
-        console.log(fiz)
-    }
+function secondToLast (arr){
+    return arr(arr.length -3)
 }
+console.log(secondToLast(["1", "2", "3", "4", "5"]));

78dc0b9dd6e9c65c4b0c8c8355728d1ebe13747d
Jennifer Deleon <jenndeleon404@gmail.com>
2021-10-25 08:58:59 -0500

    practicing

--- js/for_loops.js
+++ js/for_loops.js
@@ -22,7 +22,6 @@ for (var i = 1; i <= 10; i++) {
 }
 // ^^ that's my answer, below is what was shown in class
 
-// showMultiplicationTable(7);
 //var randomNumber = math.floor(Math.random() * (180) + 20);
 //if (randomNumber % 2 === 0){
 //console.log(randomNumber + " is even number");
@@ -30,7 +29,7 @@ for (var i = 1; i <= 10; i++) {
 //console.log(randomNumber + ' is an odd number.");
 //  }
 //}
-// ^^^^ That answer uses the showMultiplication from the previous question like I was supposed to.
+
 var x = "";
 for (var more = 1; more <= 9; more++) {
     for (var i = 1; i <= more; i++){
@@ -42,4 +41,31 @@ for (var more = 1; more <= 9; more++) {
 //5.
 for (var ok = 100; ok >= 5; ok-= 5) {
          console.log(ok);
-     }
+}
+
+for (let i = 0; i < 10; i++){
+
+}
+console.log(i)
+
+
+// Practice on same assigment to see if I can get done faster
+
+//1.
+//Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+function showMultiplicationTables(num){
+    for (var x = 1; x <= 10; x++){
+        var result = x * num;
+        console.log(num + " * " + x + " = " + result);
+    }
+}
+showMultiplicationTables(8);
+
+
+//2.
+//Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+
+for (var y = 20; y <= 200; y ++){
+
+}

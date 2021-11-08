6540d4105b40236e7f03da77e1781f6aedf04d0c
Jennifer Deleon <91964802+JennDeleon404@users.noreply.github.com>
2021-11-08 09:22:44 -0600

    pizza done

--- js/order-pizza.js
+++ js/order-pizza.js
@@ -2,3 +2,6 @@ $('#myTab a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+
+//I was going to use this JS page to build up functionality, but I didn't have enough time. I might do that later.

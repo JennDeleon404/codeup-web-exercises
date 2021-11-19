ad4fe1b747041cfcd95f1895be69970286b2273e
Jennifer Deleon <91964802+JennDeleon404@users.noreply.github.com>
2021-11-19 13:55:47 -0600

    done

--- js/jquery.js
+++ js/jquery.js
@@ -2,16 +2,16 @@
 
 $(document).ready(function() {
     // alert("Done loading DOM");
-    $('.codeup').css({"border-color": "red",
-        "border-width":"1px",
-        "border-style":"solid"
-    });
-    // $('#whatever').css('color','purple');
-    // $('h1, p, li').css('background-color', 'lightblue');
-    // $('#planets').css({
-    //     fontSize: "20px",
-    //     color: "red"
+    // $('.codeup').css({"border-color": "red",
+    //     "border-width":"1px",
+    //     "border-style":"solid"
     // });
+    $('#whatever').css('color','purple');
+    $('h1, p, li').css('background-color', 'lightblue');
+    $('#planets').css({
+        fontSize: "20px",
+        color: "red"
+    });
 });
 
 

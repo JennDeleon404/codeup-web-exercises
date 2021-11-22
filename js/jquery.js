f21e46acc93d14558a526c339d3f28804a547287
Jennifer Deleon <91964802+JennDeleon404@users.noreply.github.com>
2021-11-22 16:56:24 -0600

    finished konami

--- js/jquery.js
+++ js/jquery.js
@@ -1,21 +1,2 @@
 "use strict"
 
-// alert($('h1').html());
-//
-
-// $(document).ready(function() {
-//     // alert("Done loading DOM");
-//     // $('.codeup').css({"border-color": "red",
-//     //     "border-width":"1px",
-//     //     "border-style":"solid"
-//     // });
-//     $('#whatever').css('color','purple');
-//     $('h1, p, li').css('background-color', 'lightblue');
-//     $('#planets').css({
-//         fontSize: "20px",
-//         color: "red"
-//     });
-// });
-
-
-

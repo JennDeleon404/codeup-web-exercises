74f9d5a3e7d4a4c7bb7fe993fbd43b6119a7e0c1
Jennifer Deleon <91964802+JennDeleon404@users.noreply.github.com>
2021-11-22 10:18:19 -0600

    need to fix j-query exercise last line of function

--- js/jquery.js
+++ js/jquery.js
@@ -1,21 +1,21 @@
 "use strict"
 
-alert($('h1').html());
+// alert($('h1').html());
+//
 
-
-$(document).ready(function() {
-    // alert("Done loading DOM");
-    // $('.codeup').css({"border-color": "red",
-    //     "border-width":"1px",
-    //     "border-style":"solid"
-    // });
-    $('#whatever').css('color','purple');
-    $('h1, p, li').css('background-color', 'lightblue');
-    $('#planets').css({
-        fontSize: "20px",
-        color: "red"
-    });
-});
+// $(document).ready(function() {
+//     // alert("Done loading DOM");
+//     // $('.codeup').css({"border-color": "red",
+//     //     "border-width":"1px",
+//     //     "border-style":"solid"
+//     // });
+//     $('#whatever').css('color','purple');
+//     $('h1, p, li').css('background-color', 'lightblue');
+//     $('#planets').css({
+//         fontSize: "20px",
+//         color: "red"
+//     });
+// });
 
 
 
